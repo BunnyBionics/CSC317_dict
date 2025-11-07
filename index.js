be35eb593a73a6869ad2c6fe942fb1aac567300c
Morgan <morgan@Barad-dr.localdomain>
2025-11-06 23:16:34 -0800

    fixed paths

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 const express = require("express");
-
+const path = require('path');
 const app = express();
 const port = 3000;
 
-app.use(express.static('public'));
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.get("/", (req, res) => {
-    res.sendFile(__dirname + 'index.html');
+    const filePath = path.join(__dirname, 'public', 'index.html'); 
+        res.sendFile(filePath);
 });
 
 app.listen(port, () => {
