fe1503f7be9ead425bd61ba56fa3e9ad49a9a1cf
Morgan <morgan@Barad-dr.localdomain>
2025-11-06 22:29:19 -0800

    applied more css

--- public/app.js
+++ public/app.js
@@ -1,65 +1,66 @@
 document.getElementById("def_form").addEventListener("submit", function (event) {
   event.preventDefault();
   const input = document.getElementById("lookup").value;
+  if (input == "") {
+    warning("Please type in the box before pressing Submit.");
+    return;
+  }
   fetch_definition(input);
 });
 
 async function fetch_definition(word) {
-  const response = await fetch(`https://api.dictionaryapi.dev/api/v2/entries/en/${word}`);
-  const data = await response.json();
-  
-  let definitions = data[0].meanings[0].definitions[0].definition;
+  try {
+    const response = await fetch(`https://api.dictionaryapi.dev/api/v2/entries/en/${word}`);
+    if (response.status === 404) {
+      warning("Word not found.");
+      return;
+    }
+    const data = await response.json();
+    render_response(data);
+  } catch (error) {
+    warning(error.message);
+  }
+}
 
-  render_response(data);
-};
+function warning(message) {
+  let display = document.getElementById("display");
+  while (display.firstChild) {
+    display.removeChild(display.firstChild);
+  }
+  let warning = document.createElement("p");
+  warning.innerText = message;
+  display.appendChild(warning);
+}
 
 function render_response(data) {
-  let display = document.getElementById('display');
+  let display = document.getElementById("display");
   while (display.firstChild) {
     display.removeChild(display.firstChild);
   }
 
-  let word_header = document.createElement('h2');
+  let word_header = document.createElement("h2");
   defined_word = data[0].word;
   defined_word = defined_word.charAt(0).toUpperCase() + defined_word.substring(1); // title case for the defined word
   word_header.textContent = defined_word;
   display.appendChild(word_header);
 
-  let pronunciation_header = document.createElement('h3');
+  let pronunciation_header = document.createElement("h3");
   let pronunciation = data[0].phonetics[0].text;
   pronunciation_header.textContent = pronunciation;
   display.appendChild(pronunciation_header);
-  
+
   for (let meaning of data[0].meanings) {
-    let speech_header = document.createElement('h4');
+    let speech_header = document.createElement("h4");
     speech_header.textContent = meaning.partOfSpeech;
     display.appendChild(speech_header);
 
-    speech_list = document.createElement('ol');
+    speech_list = document.createElement("ol");
     for (let def of meaning.definitions) {
-        let def_li = document.createElement('li');
-        def_li.textContent = def.definition;
-        speech_list.appendChild(def_li);
+      let def_li = document.createElement("li");
+      def_li.textContent = def.definition;
+      speech_list.appendChild(def_li);
     }
 
     display.appendChild(speech_list);
-    
-    
-
   }
-
-//   for (let meaning of data[0].meanings)
-  
-        // const newParagraph = document.createElement('p');
-
-        // // Add text content to the paragraph
-        // newParagraph.textContent = 'This paragraph was dynamically added by JavaScript.';
-
-        // // Get the target element (the div with id 'contentArea')
-        // const contentArea = document.getElementById('contentArea');
-
-        // // Append the new paragraph to the target element
-        // contentArea.appendChild(newParagraph);
-  
-
-};
+}
